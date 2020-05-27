d4cf999e01a2dfd68eebbdb9e74edad69beaa9ae
Paulina Weiher <45978460+theCieniu@users.noreply.github.com>
2020-05-27 11:42:19 +0200

    mobile burger menu

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//document.addEventListener("scroll", alert("first test"))
+let burger = document.getElementById("burger");
+let menu = document.querySelector(".nav-links");
+let b1 = document.getElementById("b1");
+let b2 = document.getElementById("b2");
+let b3 = document.getElementById("b3");
+
+burger.addEventListener("click", () => {
+  menu.classList.toggle("mobile-menu");
+  b1.classList.toggle("b1");
+  b2.classList.toggle("b2");
+  b3.classList.toggle("b3");
+})
